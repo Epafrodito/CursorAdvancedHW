7177f5da7cfd13121e59afb905b6552148639cbf
Oleh Mosiichuk <epafrodito17@gmail.com>
2022-02-24 00:17:02 +0200

    add HW#2

---
+++ HW#2 Цикли і розгалуження/script.js
@@ -0,0 +1,37 @@
+let numberFromUserN = +prompt('Введіть ціле значення N');
+while (!Number.isInteger(numberFromUserN)) {
+    numberFromUserN = +prompt('Введіть ціле значення N');
+}
+
+console.log(numberFromUserN);
+
+let numberFromUserM = +prompt('Введіть ціле значення M');
+while (!Number.isInteger(numberFromUserM) || (numberFromUserM < numberFromUserN)) {
+    numberFromUserM = +prompt('Введіть ціле значення M');
+}
+
+console.log(numberFromUserM);
+
+let withOutEven = confirm('Пропускати парні числа?');
+
+let result = 0;
+let i = numberFromUserN;
+
+do {
+  console.log(i);
+  if (withOutEven) {
+      if ( i % 2 != 0 ) {
+      result += i;
+  }} else { 
+  result = result + i}
+  i += 1;
+} while (i <= numberFromUserM)
+
+console.log(result);
+
+document.writeln (`
+    Значення числа N: ${numberFromUserN} <br>
+    Значення числа M: ${numberFromUserM} <br>
+    Пропускати парні числа: ${withOutEven} <br>
+    Сума: ${result}`
+)
